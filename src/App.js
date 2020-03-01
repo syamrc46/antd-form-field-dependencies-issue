891a266bebc2b2c47cc9e9e3c235eef260b00f8c
Syam Prasad <syamrc46@gmail.com>
2020-03-01 17:38:20 +0530

    code cleaning

--- src/App.js
+++ src/App.js
@@ -3,66 +3,64 @@ import { Form, Button, Input } from 'antd';
 import 'antd/dist/antd.css';
 
 export default function App(props) {
-	{
-		const [ form ] = Form.useForm();
+	const [ form ] = Form.useForm();
 
-		const initialValues = {
-			security: {
-				password: null,
-				confirm: null
-			}
-		};
+	const initialValues = {
+		security: {
+			password: null,
+			confirm: null
+		}
+	};
 
-		return (
-			<Form
-				form={form}
-				initialValues={initialValues}
-				onFinish={() => {
-					const { validateFields } = form;
-					validateFields()
-						.then((values) => {
-							console.log('Submitted values', values);
-						})
-						.catch((ex) => {
-							console.log('Error occured');
-						});
-				}}
+	return (
+		<Form
+			form={form}
+			initialValues={initialValues}
+			onFinish={() => {
+				const { validateFields } = form;
+				validateFields()
+					.then((values) => {
+						console.log('Submitted values', values);
+					})
+					.catch((ex) => {
+						console.log('Error occured');
+					});
+			}}
+		>
+			<Form.Item
+				name={[ 'security', 'password' ]}
+				label="Password"
+				rules={[
+					{
+						required: true,
+						message: 'Password is required'
+					}
+				]}
 			>
-				<Form.Item
-					name={[ 'security', 'password' ]}
-					label="Password"
-					rules={[
-						{
-							required: true,
-							message: 'Password is required'
-						}
-					]}
-				>
-					<Input />
-				</Form.Item>
-				<Form.Item
-					name={[ 'security', 'confirm' ]}
-					label="Confirm"
-					dependencies={['security', 'password' ]}
-					rules={[
-						{
-							required: true,
-							message: 'Confirm password is required'
-						},
-						({ getFieldValue }) => ({
-							validator(rule, value) {
-								if (!value || getFieldValue([ 'security', 'password' ]) === value) {
-									return Promise.resolve();
-								}
-								return Promise.reject('The two passwords that you entered do not match!');
+				<Input />
+			</Form.Item>
+			<Form.Item
+				name={[ 'security', 'confirm' ]}
+				label="Confirm"
+				dependencies={[ 'security', 'password' ]}
+				rules={[
+					{
+						required: true,
+						message: 'Confirm password is required'
+					},
+					({ getFieldValue }) => ({
+						validator(rule, value) {
+							if (!value || getFieldValue([ 'security', 'password' ]) === value) {
+								return Promise.resolve();
 							}
-						})
-					]}
-				>
-					<Input />
-				</Form.Item>
-				<Button htmlType="submit">Submit</Button>
-			</Form>
-		);
-	}
+							return Promise.reject('The two passwords that you entered do not match!');
+						}
+					})
+				]}
+			>
+				<Input />
+			</Form.Item>
+			<Button htmlType="submit">Submit</Button>
+		</Form>
+	);
 }
